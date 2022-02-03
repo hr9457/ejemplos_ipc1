e91ad0ffc7ee04851cc3cab7ae9a5ab378621c93
hector <hr9457@gmail.com>
2022-02-03 11:28:38 -0600

    ejemplos libreria scanner

--- PrimerPrograma/src/primerprograma/PrimerPrograma.java
+++ PrimerPrograma/src/primerprograma/PrimerPrograma.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package primerprograma;
+import java.util.Scanner;
 
 /**
  *
@@ -17,17 +18,63 @@ public class PrimerPrograma {
 //    int numero = 12;
     
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola mundo!!!");
-        int numero = 12;
         
-        if(numero % 2 == 0){
-            System.out.println("Es un numero par");
-        }else{
-            System.out.println("No es un numero par");
+        int i  = 8;
+        int j = 8;
+        int[][] pacman = new int[i][j];
+        
+        pacman[0][0] = 1;
+        
+        for(int fila=0; i < fila; i++){
+            for(int columna=0; j<columna; j++){
+                System.out.println(pacman[fila][columna]);
+            }
         }
         
-        System.out.println("--PACMAN--");
+        
+//        int[] notas = new int[3];
+//        notas[0] = 8;
+//        notas[2] = 20;
+//        System.out.println(notas[2]);
+
+    
+//      arrreglos para mi historial
+//        String[] nombre = new String[5];
+//        int[] puntaje = new int[5];
+//        int[] movimientos = new int[5];
+//        
+//        nombre[3] = "Hector";        
+//        puntaje[3] = 100;
+//        movimientos[3] = 100;
+//        
+//        System.out.println(nombre.length);
+        
+//        impresion de mi historial
+//        for(int  i = 0; i <= nombre.length - 1; i++){
+//            if(puntaje[i] == 0){
+//                // nada                
+//            } else {
+//                System.out.println("Nombre del jugador: " + nombre[i]);
+//                System.out.println(puntaje[i]);
+//                System.out.println(movimientos[i]);
+//            }            
+//        }
+//        
+        
+        
+        // TODO code application logic here
+//        System.out.println("Hola mundo!!!");
+//        int numero = 12;
+//        
+//        if(numero % 2 == 0){
+//            System.out.println("Es un numero par");
+//        }else{
+//            System.out.println("No es un numero par");
+//        }
+//        
+//        System.out.println("--PACMAN--");
+
+    
     }
     
 }
