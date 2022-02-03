afb9cba95ca50a0949b50f5eae04e6bc3f150095
hector <hr9457@gmail.com>
2022-02-03 09:17:48 -0600

    menu realizado

---
+++ PrimerPrograma/src/primerprograma/PrimerPrograma.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primerprograma;
+
+/**
+ *
+ * @author joshu
+ */
+public class PrimerPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+//    int numero = 12;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo!!!");
+        int numero = 12;
+        
+        if(numero % 2 == 0){
+            System.out.println("Es un numero par");
+        }else{
+            System.out.println("No es un numero par");
+        }
+    }
+    
+}
